813f329644efde58078525dafa1fea8e0c3762a1
mohamedmansy316 <mohamedmansy855@gamil.com>
2021-01-10 00:20:45 +0200

    add xbox design

---
+++ Xbox/Xbox/Scripts/JavaScript.js
@@ -0,0 +1,70 @@
+﻿$("#More").click(function () {
+
+    $(".notification").fadeIn("slow");
+        $(".notification").css("display", "flex")
+
+   
+        if ($(".notification-hide").css("display", "none")) {
+            $("#More").click(function () {
+                $(".notification-hide").css("display", "flex")
+            })
+        }
+});
+
+$("#Less").click(function () {
+
+    $(".notification").fadeOut("slow");
+    $(".notification").css("display", "none")
+
+
+    if ($(".notification-hide").css("display", "flex")) {
+        $("#Less").click(function () {
+            $(".notification-hide").css("display", "none")
+        })
+    }
+});
+
+$(".option-show1").click(function () {
+
+    $(".box1").fadeToggle(500);
+    $(".box2 , .box3 , .box4 , .box5 , .box6 , .box7").fadeOut();
+
+});
+$(".option-show2").click(function () {
+
+    $(".box2").fadeToggle(500);
+    $(".box1 , .box3, .box4 , .box5 , .box6 , .box7").fadeOut();
+   
+
+});
+$(".option-show3").click(function () {
+
+    $(".box3").fadeToggle(500);
+    $(".box2 , .box1 , .box4 , .box5 , .box6 , .box7").fadeOut();
+
+})
+$(".option-show4").click(function () {
+
+    $(".box4").fadeToggle(500);
+    $(".box2 , .box3 , .box1 , .box5 , .box6 , .box7").fadeOut();
+
+})
+$(".option-show5").click(function () {
+
+    $(".box5").fadeToggle(500);
+    $(".box2 , .box3 , .box4 , .box1 , .box6 , .box7").fadeOut();
+
+
+})
+$(".option-show6").click(function () {
+
+    $(".box6").fadeToggle(500);
+    $(".box2 , .box3 , .box4 , .box5 , .box1 , .box7").fadeOut();
+
+})
+$(".option-show7").click(function () {
+
+    $(".box7").fadeToggle(500);
+    $(".box2 , .box3 , .box4 , .box5 , .box6 , .box1").fadeOut();
+
+})
